c73afe362d86662ded73c9e3b28fa9c61d350625
Rafli <21523042@students.uii.ac.id>
2026-01-31 18:57:27 +0700

    perbaikan kodingan di main.py lagi.5

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def calculate_circadian_stability(user_id: str = "default_user") -> float:
         return None
 
 @app.post("/predict_sleep_health")
-async def predict_sleep_health( SleepData):
+async def predict_sleep_health(data: SleepData):
     try:
         # Simpan data ke database
         try:
